cd68a6707dd45b9fa1759ed142ce3a392aff433a
Kun He <hekun@cs.bu.edu>
2016-03-01 22:44:03 -0500

    train_osh_rs x NUSWIDE

--- train_osh_rs.m
+++ train_osh_rs.m
@@ -33,15 +33,15 @@ function [train_time, update_time, bitflips] = train_sgd_rs(...
 		return;
 	end
 
-	% randomly generate candidate codewords, store in M2
+	% randomly generate candidate codewords, store in ECOCs
 	bigM = 10000;
-	M2   = zeros(bigM, opts.nbits);
+	ECOCs = zeros(bigM, opts.nbits);
 	for t = 1:opts.nbits
 		r = ones(bigM, 1);
 		while (abs(sum(r)) == bigM)
 			r = 2*randi([0,1], bigM, 1)-1;
 		end
-		M2(:, t) = r;
+		ECOCs(:, t) = r;
 	end
 	clear r
 
@@ -64,30 +64,52 @@ function [train_time, update_time, bitflips] = train_sgd_rs(...
 	Yres           = [];  % mapped binary codes for the reservoir
 
 	i_ecoc = 1;
+	M_ecoc = [];
 	seenLabels = [];
 	for i = 1:opts.noTrainingPoints
 		t_ = tic;
+		skip_update = false;
 		% new training point
 		spoint = traingist(i, :);
-		slabel = trainlabels(i);
+		slabel = trainlabels(i, :);
 
 		% check whether it exists in the "seen class labels" vector
-		islabel = find(seenLabels == slabel);
-		if isempty(islabel)
-			if isempty(seenLabels)
-				% does not exist, create a binary code for M
-				seenLabels = slabel;
-				M = M2(i_ecoc, :);
-				i_ecoc = i_ecoc + 1;
-			else
-				% append codeword to ECOC matrix
-				seenLabels = [seenLabels; slabel];
-				M = [M; M2(i_ecoc,:)];
-				i_ecoc = i_ecoc +1;
+		if length(slabel) == 1  % single-label dataset
+			islabel = find(seenLabels == slabel);
+			if isempty(islabel)
+				if isempty(seenLabels)
+					% does not exist, create a binary code for M_ecoc
+					seenLabels = slabel;
+					M_ecoc = ECOCs(i_ecoc, :);
+					i_ecoc = i_ecoc + 1;
+				else
+					% append codeword to ECOC matrix
+					seenLabels = [seenLabels; slabel];
+					M_ecoc = [M_ecoc; ECOCs(i_ecoc,:)];
+					i_ecoc = i_ecoc +1;
+				end
 			end
+			islabel = find(seenLabels == slabel);
+		else  % multi-label dataset
+			if sum(slabel) == 0, skip_update = true; end
+			if isempty(seenLabels), 
+				seenLabels = zeros(size(slabel)); 
+			end
+
+			% if not all incoming labels are seen
+			inds = find(slabel == 1);
+			seen = seenLabels(inds);
+			if sum(seen) < length(seenLabels)
+				seenLabels(inds) = 1;
+				for j = find(seen == 0)
+					M_ecoc = [M_ecoc; ECOCs(inds(j), :)];
+					indexRank(i_ecoc) = inds(j);
+					i_ecoc = i_ecoc+1;
+				end
+			end
+			islabel = find(ismember(indexRank, inds));
 		end
-		islabel = find(seenLabels == slabel);
-		target_code = M(islabel, :);
+		target_codes = M_ecoc(islabel, :);
 
 		% reservoir update (based on random sort)
 		priority_queue = zeros(1, reservoir_size);
@@ -107,9 +129,9 @@ function [train_time, update_time, bitflips] = train_sgd_rs(...
 			end
 			% compute binary codes for the reservoir
 			if isempty(Yres)
-				Yres = build_hash_table(W, samplegist, samplelabel, seenLabels, M, opts)';
+				Yres = build_hash_table(W, samplegist, samplelabel, seenLabels, M_ecoc, opts)';
 			else
-				Ynew = build_hash_table(W, samplegist, samplelabel, seenLabels, M, opts)';
+				Ynew = build_hash_table(W, samplegist, samplelabel, seenLabels, M_ecoc, opts)';
 				bitdiff = (Yres ~= Ynew);
 				bitflips_res = bitflips_res + sum(bitdiff(:));
 				Yres = Ynew;
@@ -117,37 +139,45 @@ function [train_time, update_time, bitflips] = train_sgd_rs(...
 		end
 
 		% hash function update
-		if opts.SGDBoost == 0
-			% vectorized updates
-			if i <= reservoir_size
-				W = sgd_update_hinge(W, spoint, target_code, opts.stepsize);
-			else
-				codes  = [target_code; Yres];
-				points = [spoint; samplegist*(opts.lambda/reservoir_size)];
-				stepsizes = opts.stepsize * [1; ones(reservoir_size,1)*opts.lambda/reservoir_size];
-				W = sgd_update_hinge(W, points, codes, stepsizes);
-			end
-		else
-			% TODO
-			for j = 1:opts.nbits
-				if j ~= 1
-					c1 = exp(-(target_code(1:j-1)*(W(:,1:j-1)'*spoint')));
-				else
-					c1 = 1;
+		if ~skip_update
+			if opts.SGDBoost == 0
+				for c = 1:size(target_codes, 1)
+					code = target_codes(c, :);
+					% vectorized updates
+					if i <= reservoir_size
+						W = sgd_update_hinge(W, spoint, code, opts.stepsize);
+					else
+						codes  = [code; Yres];
+						points = [spoint; samplegist*(opts.lambda/reservoir_size)];
+						stepsizes = opts.stepsize * [1; ones(reservoir_size,1)*opts.lambda/reservoir_size];
+						W = sgd_update_hinge(W, points, codes, stepsizes);
+					end
 				end
-				W(:,j) = W(:,j) - opts.stepsize * ...
-					c1 * exp(-target_code(j)*W(:,j)'*spoint')*-target_code(j)*spoint';
+			else
+				for c = 1:size(target_codes, 1)
+					code = target_codes(c, :);
+					% TODO vectorize
+					for j = 1:opts.nbits
+						if j ~= 1
+							c1 = exp(-(code(1:j-1)*(W(:,1:j-1)'*spoint')));
+						else
+							c1 = 1;
+						end
+						W(:,j) = W(:,j) - opts.stepsize * ...
+							c1 * exp(-code(j)*W(:,j)'*spoint')*-code(j)*spoint';
+					end
+				end
 			end
+			train_time = train_time + toc(t_);
 		end
-		train_time = train_time + toc(t_);
 
 		% hash index update
 		if strcmp(opts.mapping, 'smooth') && ~mod(i, opts.update_interval)
 			t_ = tic;
 			if isempty(Y)
-				Y = build_hash_table(W, traingist, trainlabels, seenLabels, M, opts);
+				Y = build_hash_table(W, traingist, trainlabels, seenLabels, M_ecoc, opts);
 			else
-				Ynew = build_hash_table(W, traingist, trainlabels, seenLabels, M, opts);
+				Ynew = build_hash_table(W, traingist, trainlabels, seenLabels, M_ecoc, opts);
 				bitdiff = (Y ~= Ynew);
 				bitflips = bitflips + sum(bitdiff(:));
 				Y = Ynew;
@@ -158,7 +188,7 @@ function [train_time, update_time, bitflips] = train_sgd_rs(...
 		% cache intermediate model to disk
 		if ~mod(i, opts.test_interval)
 			if isempty(Y)
-				Y = build_hash_table(W, traingist, trainlabels, seenLabels, M, opts);
+				Y = build_hash_table(W, traingist, trainlabels, seenLabels, M_ecoc, opts);
 			end
 			savefile = sprintf('%s_iter%d.mat', prefix, i);
 			save(savefile, 'W', 'Y', 'bitflips', 'train_time', 'update_time');
@@ -170,7 +200,7 @@ function [train_time, update_time, bitflips] = train_sgd_rs(...
 
 	% populate hash table
 	t_ = tic;
-	Y = build_hash_table(W, traingist, trainlabels, seenLabels, M, opts);
+	Y = build_hash_table(W, traingist, trainlabels, seenLabels, M_ecoc, opts);
 	update_time = update_time + toc(t_);
 	myLogInfo('Trial %02d. bitflips_res=%.2f, SGD+reservoir: %.2f sec, Hashtable update: %.2f sec', ...
 		trialNo, bitflips_res, train_time, update_time);
