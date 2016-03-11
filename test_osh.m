fcakir/mihash

commit cad18f1df4b7bedd81853a2c1b245754872f7ffa

space saving w logical

get_opts.m
@@ -21,20 +21,20 @@ function opts = get_opts(dataset, nbits, varargin)
 	ip.addParamValue('stepsize', 0.1, @isscalar);
 	ip.addParamValue('SGDBoost', 0, @isscalar);
 	ip.addParamValue('randseed', 12345, @isscalar);
-	ip.addParamValue('localdir', '/scratch/online-hashing', @isstr);
-
+	ip.addParamValue('localdir', ...
+		'/research/object_detection/cachedir/online-hashing', @isstr);
 	ip.addParamValue('noTrainingPoints', 2000, @isscalar);
 	ip.addParamValue('override', 0, @isscalar);
 
 	% controling when to update hash table
 	% default: save every opts.update_interval iterations
 	% IF use reservoir AND opts.flip_thresh > 0, THEN use opts.flip_thresh
-	ip.addParamValue('update_interval', 200, @isscalar); % use with baseline
-	ip.addParamValue('flip_thresh', 10, @isscalar); % use with reservoir
+	ip.addParamValue('update_interval', 100, @isscalar); % use with baseline
+	ip.addParamValue('flip_thresh', -1, @isscalar); % use with reservoir
 
 	% testing
-	ip.addParamValue('test_interval', -1, @isscalar);   % save intermediate model
-	ip.addParamValue('test_frac', 1, @isscalar);         % for faster testing
+	%ip.addParamValue('test_interval', -1, @isscalar);   % save intermediate model
+	ip.addParamValue('test_frac', 1, @isscalar);         % <1 for faster testing
 	ip.addParamValue('ntests', 10, @isscalar);
 	
 	%ip.addParamValue('exp', 'baseline', @isstr);   % baseline, rs, l1l2
@@ -52,13 +52,12 @@ function opts = get_opts(dataset, nbits, varargin)
 	% assertions
 	assert(~(opts.reg_maxent>0 && opts.reg_smooth>0));  % can't have both
 	assert(opts.test_frac > 0);
-	assert(opts.test_interval <= opts.noTrainingPoints, ... 
-		'test_interval should be smaller than \# of training points');
+	%assert(opts.test_interval <= opts.noTrainingPoints, ... 
+		%'test_interval should be smaller than \# of training points');
 
 	% make localdir
 	if ~exist(opts.localdir, 'dir'), 
-		mkdir(opts.localdir); 
-		unix(['chmod g+rw ' opts.localdir]);
+		mkdir(opts.localdir);  unix(['chmod g+rw ' opts.localdir]);
 	end
 
 	% set randseed -- don't change the randseed if don't have to!
@@ -87,7 +86,7 @@ function opts = get_opts(dataset, nbits, varargin)
 		opts.identifier = sprintf('%s-ME%g', opts.identifier, opts.reg_maxent);
 	end
 	if opts.reg_smooth > 0
-		; %TODO
+		opts.identifier = sprintf('%s-SM%g', opts.identifier, opts.reg_smooth);
 	end
 
 	% set expdir

load_cnn.m
@@ -1,43 +1,33 @@
-function [traincnn, trainlabels, testcnn, testlabels, cateTrainTest, opts] = ...
+function [Xtrain, Ytrain, Xtest, Ytest, cateTrainTest, opts] = ...
 		load_cnn(dataset, opts)
 
 	tic;
 	if strcmp(dataset, 'cifar')
-		load('/research/codebooks/hashing_project/data/cifar-10/descriptors/cnn.mat');
-		cnn        = [traincnn; testcnn];
-		cnnlabels  = [trainlabels; testlabels];
-		tstperclass = 100;
-		%opts.noTrainingPoints = 2000;  % # points used for training
-		[traincnn, trainlabels, testcnn, testlabels, cateTrainTest] = ...
+		% TODO
+		[Xtrain, Ytrain, Xtest, Ytest, cateTrainTest] = ...
 			split_train_test(cnn, cnnlabels, tstperclass);
 
 	elseif strcmp(dataset, 'sun')
-		load('/research/codebooks/hashing_project/data/sun397/SUN_cnn.mat');
-		cnnlabels  = labels;
-		tstperclass = 10;
-		%opts.noTrainingPoints = 3970;  % # points used for training
-		[traincnn, trainlabels, testcnn, testlabels, cateTrainTest] = ...
+		% TODO
+		[Xtrain, Ytrain, Xtest, Ytest, cateTrainTest] = ...
 			split_train_test(cnn, cnnlabels, tstperclass);
 
 	elseif strcmp(dataset, 'nus')
-    cnn = load('/research/codebooks/hashing_project/data/nuswide/BoW_int.dat');
-    tags = load('/research/codebooks/hashing_project/data/nuswide/AllLabels81.txt');
-    tstperclass = 30;
-		%opts.noTrainingPoints = 20*81;
-		[traincnn, trainlabels, testcnn, testlabels, cateTrainTest] = ...
+		% TODO
+		[Xtrain, Ytrain, Xtest, Ytest, cateTrainTest] = ...
 			split_train_test_nus(cnn, tags, tstperclass);
 
 	elseif strcmp(dataset, 'places2')
 		% TODO
 
-	else, error('unknown dataset'); end
+	else, error(['unknown dataset: ' dataset]); end
 
-	whos traincnn trainlabels testcnn testlabels cateTrainTest
+	whos Xtrain Ytrain Xtest Ytest cateTrainTest
 	myLogInfo('Dataset "%s" loaded in %.2f secs', dataset, toc);
 end
 
 % --------------------------------------------------------
-function [traincnn, trainlabels, testcnn, testlabels, cateTrainTest] = ...
+function [Xtrain, Ytrain, Xtest, Ytest, cateTrainTest] = ...
 		split_train_test(cnn, cnnlabels, tstperclass)
 
 	% normalize features
@@ -50,57 +40,29 @@ function [traincnn, trainlabels, testcnn, testlabels, cateTrainTest] = ...
 	testsize    = num_classes * tstperclass;
 	trainsize   = size(cnn, 1) - testsize;
 	cnndim     = size(cnn, 2);
-	testcnn    = zeros(testsize, cnndim);
-	testlabels  = zeros(testsize, 1);
-	traincnn   = zeros(trainsize, cnndim);
-	trainlabels = zeros(trainsize, 1);
+	Xtest    = zeros(testsize, cnndim);
+	Ytest  = zeros(testsize, 1);
+	Xtrain   = zeros(trainsize, cnndim);
+	Ytrain = zeros(trainsize, 1);
 	count = 0;
 	for i = 1:num_classes
 		ind = find(cnnlabels == clslbl(i));
 		n_i = length(ind);
 		ind = ind(randperm(n_i));
-		testcnn((i-1)*tstperclass+1:i*tstperclass,:) = cnn(ind(1:tstperclass),:);
-		testlabels((i-1)*tstperclass+1:i*tstperclass) = clslbl(i);
-		traincnn(count+1:count+n_i-tstperclass, :)   = cnn(ind(tstperclass+1:end),:);
-		trainlabels(count+1:count+n_i-tstperclass, :) = clslbl(i);
+		Xtest((i-1)*tstperclass+1:i*tstperclass,:) = cnn(ind(1:tstperclass),:);
+		Ytest((i-1)*tstperclass+1:i*tstperclass) = clslbl(i);
+		Xtrain(count+1:count+n_i-tstperclass, :)   = cnn(ind(tstperclass+1:end),:);
+		Ytrain(count+1:count+n_i-tstperclass, :) = clslbl(i);
 		count = count + n_i - tstperclass;
 	end
 	% randomize again
-	ind         = randperm(size(traincnn, 1));
-	traincnn   = traincnn(ind, :);
-	trainlabels = trainlabels(ind);
-	ind         = randperm(size(testcnn, 1));
-	testcnn    = testcnn(ind, :);
-	testlabels  = testlabels(ind);
+	ind         = randperm(size(Xtrain, 1));
+	Xtrain   = Xtrain(ind, :);
+	Ytrain = Ytrain(ind);
+	ind         = randperm(size(Xtest, 1));
+	Xtest    = Xtest(ind, :);
+	Ytest  = Ytest(ind);
 
-	cateTrainTest = repmat(trainlabels, 1, length(testlabels)) ...
-		== repmat(testlabels, 1, length(trainlabels))';
-end
-
-% --------------------------------------------------------
-function [traincnn, traintags, testcnn, testtags, cateTrainTest] = ...
-		split_train_test_nus(cnn, tags, tstperclass)
-
-	% normalize features
-	cnn = bsxfun(@minus, cnn, mean(cnn,1));  % first center at 0
-	cnn = normalize(cnn);  % then scale to unit length
-
-	% construct test and training set
-	num_classes = 81;
-	testsize    = num_classes * tstperclass;
-	ind         = randperm(size(cnn, 1));
-	testcnn    = cnn(ind(1:testsize), :);
-	testtags    = tags(ind(1:testsize), :);
-	traincnn   = cnn(ind(testsize+1:end), :);
-	traintags   = tags(ind(testsize+1:end), :);
-
-	% randomize again
-	ind       = randperm(size(traincnn, 1));
-	traincnn = traincnn(ind, :);
-	traintags = traintags(ind, :);
-	ind       = randperm(size(testcnn, 1));
-	testcnn  = testcnn(ind, :);
-	testtags  = testtags(ind, :);
-
-	cateTrainTest = (traintags * testtags' > 0);
+	cateTrainTest = repmat(Ytrain, 1, length(Ytest)) ...
+		== repmat(Ytest, 1, length(Ytrain))';
 end

commit 619ca38f0630bf36258080322299466295de01db

BIG bugfix + improvements
fixes bug introduced in commit cad18f1d: space saving w/ logical

bug description:
- candidate codeword matrix ECOCs was changed to logical format (therefore
a 0/1 matrix) instead of the original -1/1 matrix
- but in updating codeword matrix M_ecoc, it's not converted back to
  -1/1 format, therefore leaving codewords in 0/1 format
- in SGD codewords are expected to be in -1/1 format

additional bugfix in get_results
- when using binary search, prec_k gives wrong results (TODO)

improvements
+ Xtrain, etc. are now global variables to save loading time
+ improved logging in sgd_optim

demo_osh.m
@@ -55,27 +55,27 @@ function resfn = demo_osh(ftype, dataset, nbits, varargin)
 
 
 	% 2. load data (only if necessary)
-	if any(run_trial) || ~all(res_exist)
-		myLogInfo('Loading data...');
+	global Xtrain Xtest Ytrain Ytest Dtype
+	Dtype_this = [dataset '_' ftype];
+	if strcmp(Dtype_this, Dtype)
+		myLogInfo('Dataset already loaded for %s', Dtype_this);
+	elseif isempty(Dtype) && (any(run_trial) || ~all(res_exist))
+		myLogInfo('Loading data for %s...', Dtype_this);
 		eval(['[Xtrain, Ytrain, Xtest, Ytest] = load_' opts.ftype '(dataset, opts);']);
+		Dtype = Dtype_this;
 	end
 
 	% 3. TRAINING: run all _necessary_ trials (handled by train_osh)
 	if any(run_trial)
 		myLogInfo('Training models...');
-		train_osh(Xtrain, Ytrain, run_trial, opts);
+		train_osh(run_trial, opts);
 	end
+	myLogInfo('Training is done.');
 
 	% 4. TESTING: run all _necessary_ trials
 	if ~all(res_exist)
 		myLogInfo('Testing models...');
-		% handle test_frac
-		if opts.test_frac < 1
-			myLogInfo('! only testing first %g%%', opts.test_frac*100);
-			idx = 1:round(size(Xtest, 1)*opts.test_frac);
-			Xtest = Xtest(idx, :);
-			Ytest = Ytest(idx);
-		end
-		test_osh(Xtest, Ytest, Ytrain, resfn, res_trial_fn, res_exist, opts);
+		test_osh(resfn, res_trial_fn, res_exist, opts);
 	end
+	myLogInfo('Testing is done.');
 end

get_opts.m
@@ -95,20 +95,33 @@ function opts = get_opts(ftype, dataset, nbits, varargin)
 	% make localdir
 	if ~exist(opts.localdir, 'dir'), 
 		mkdir(opts.localdir);  
-		if ~opts.windows, unix(['chmod -R g+rw ' opts.localdir]); end
+		if ~opts.windows, unix(['chmod g+rw ' opts.localdir]); end
 	end
 
 	% set randseed -- don't change the randseed if don't have to!
 	rng(opts.randseed);
 
-	% identifier string for the current experiment
-	if strcmp(opts.dataset, 'places')
-		% [hack] for places
-		assert(opts.labelspercls >= 500 && opts.labelspercls <= 5000, ...
-			'please give a reasonable labelspercls in [500, 5000]');
-		myLogInfo('Places will use %d labeled examples per class', opts.labelspercls);
-		opts.dataset = [opts.dataset, 'L', num2str(opts.labelspercls)];
+	% FC: if mapping is not smooth, set update_interval to noTrainingPoints
+	if ~strcmp(opts.mapping, 'smooth') && opts.update_interval > 0 && ...
+			(opts.update_interval ~= opts.noTrainingPoints)
+		myLogInfo('Mapping: %s. Overriding update_interval=%d to noTrainingPoints=%d', ...
+			opts.mapping, opts.update_interval, opts.noTrainingPoints);
+		opts.update_interval = opts.noTrainingPoints;
 	end
+
+	% [hack] for places
+	if strcmp(opts.dataset, 'places')
+		if opts.labelspercls > 0
+			assert(opts.labelspercls >= 500 && opts.labelspercls <= 5000, ...
+				'please give a reasonable labelspercls in [500, 5000]');
+			myLogInfo('Places will use %d labeled examples per class', opts.labelspercls);
+			opts.dataset = [opts.dataset, 'L', num2str(opts.labelspercls)];
+		else
+			myLogInfo('Places: fully supervised experiment');
+		end
+	end
+
+	% identifier string for the current experiment
 	opts.identifier = sprintf('%s-%s-%d%s-B%dS%g', opts.dataset, opts.ftype, ...
 		opts.nbits, opts.mapping, opts.SGDBoost, opts.stepsize);
 	if opts.reg_rs > 0
@@ -135,6 +148,7 @@ function opts = get_opts(ftype, dataset, nbits, varargin)
 	if opts.reg_smooth > 0
 		opts.identifier = sprintf('%s-SM%g', opts.identifier, opts.reg_smooth);
 	end
+	myLogInfo('identifier: %s', opts.identifier);
 
 	% set expdir
 	expdir_base = sprintf('%s/%s', opts.localdir, opts.identifier);

get_results.m
@@ -12,6 +12,7 @@ function res = get_results(Htrain, Htest, Ytrain, Ytest, opts)
 
 	if strcmp(opts.metric, 'mAP')
 		sim = single(2*Htrain-1)'*single(2*Htest-1);
+		%sim = Htrain'*Htest;
 		AP  = zeros(1, testsize);
 		parfor j = 1:testsize
 			labels = 2*double(Ytrain==Ytest(j))-1;
@@ -28,16 +29,19 @@ function res = get_results(Htrain, Htest, Ytrain, Ytest, opts)
 		K = opts.prec_k;
 		prec_k = zeros(1, testsize);
 		sim = single(2*Htrain-1)'*single(2*Htest-1);
+		%sim = Htrain'*Htest;
 
 		parfor i = 1:testsize
 			sim_i = sim(:, i);
-			th = binsearch(sim_i, K);
-			[~, I] = sort(sim_i(sim_i>th), 'descend');
+			%th = binsearch(sim_i, K);
+			%[~, I] = sort(sim_i(sim_i>th), 'descend');
+			[~, I] = sort(sim_i, 'descend');
 			I = I(1:K);
 			prec_k(i) = mean(Ytrain(I) == Ytest(i));
 		end
-		res = mean(prec_k(~isnan(prec_k)));
-		myLogInfo('Prec@%d neighbors = %g', K, res);
+		%prec_k = prec_k(~isnan(prec_k));
+		res = mean(prec_k);
+		myLogInfo('Prec@(neighs=%d) = %g', K, res);
 
 
 	elseif ~isempty(strfind(opts.metric, 'prec_n'))
@@ -45,13 +49,19 @@ function res = get_results(Htrain, Htest, Ytrain, Ytest, opts)
 		R = opts.nbits;
 		prec_n = zeros(1, testsize);
 		sim = single(2*Htrain-1)'*single(2*Htest-1);
+		%sim = Htrain'*Htest;
 
+		% NOTE 'for' has better CPU usage
 		for j=1:testsize
 			labels = (Ytrain==Ytest(j));
-			prec_n(j) = mean(labels((-sim(:,j)+R)/2 <= N));
+			ind = find(R-sim(:,j) <= 2*N);
+			if ~isempty(ind)
+				prec_n(j) = mean(labels(ind));
+			end
 		end
-		res = mean(prec_n(~isnan(prec_n)));
-		myLogInfo('Prec@%d radius = %g', N, res);
+		%prec_n = prec_n(~isnan(prec_n));
+		res = mean(prec_n);
+		myLogInfo('Prec@(radius=%d) = %g', N, res);
 
 	else
 		error(['Evaluation metric ' opts.metric ' not implemented']);

load_cnn.m
@@ -26,11 +26,12 @@ function [Xtrain, Ytrain, Xtest, Ytest] = load_cnn(dataset, opts)
 		% loads variables: pca_feats, labels, images
 		% NOTE: labels are from 0 to 204, we add 1 to each to make them 1 to 205
 		%       because we want to use 0 to indicate unlabeled
+		clear pca_feats labels images
 		load([basedir '/places/places_alexnet_fc7pca128.mat']);
-		X = double(pca_feats);
+		X = pca_feats;
 		Y = labels + 1;
-		T = 20;
-		L = opts.labelspercls;  % default 2500, range [500, 5000]
+		T = 40;
+		L = opts.labelspercls;  % default 2500, range {0}U[500, 5000]
 		% semi-supervised
 		[Xtrain, Ytrain, Xtest, Ytest] = split_train_test(X, Y, T, L);
 
@@ -48,25 +49,29 @@ function [Xtrain, Ytrain, Xtest, Ytest] = split_train_test(X, Y, T, L)
 	% L: [optional] # labels to retain per class
 	if nargin < 4, L = 0; end
 
+	% randomize
+	I = randperm(size(X, 1));
+	X = X(I, :);
+	Y = Y(I);
+
 	% normalize features
 	X = bsxfun(@minus, X, mean(X,1));  % first center at 0
-	X = normalize(X);  % then scale to unit length
-	D = size(X, 2);
+	X = normalize(double(X));  % then scale to unit length
+	D = size(X, 2)
 
 	labels = unique(Y);
-	nclass = length(labels);
-	ntest  = nclass * T;
+	ntest  = length(labels) * T;
 	ntrain = size(X, 1) - ntest;
 	Xtrain = zeros(ntrain, D);  Xtest = zeros(ntest, D);
 	Ytrain = zeros(ntrain, 1);  Ytest = zeros(ntest, 1);
 	
 	% construct test and training set
 	cnt = 0;
-	for i = 1:nclass
+	for i = 1:length(labels)
 		% find examples in this class, randomize ordering
 		ind = find(Y == labels(i));
 		n_i = length(ind);
-		ind = ind(randperm(n_i));
+		%ind = ind(randperm(n_i));
 
 		% assign test
 		Xtest((i-1)*T+1:i*T, :) = X(ind(1:T), :);
@@ -90,10 +95,10 @@ function [Xtrain, Ytrain, Xtest, Ytest] = split_train_test(X, Y, T, L)
 	end
 
 	% randomize again
-	ind    = randperm(size(Xtrain, 1));
+	ind    = randperm(ntrain);
 	Xtrain = Xtrain(ind, :);
 	Ytrain = Ytrain(ind);
-	ind    = randperm(size(Xtest, 1));
+	ind    = randperm(ntest);
 	Xtest  = Xtest(ind, :);
 	Ytest  = Ytest(ind);
 end

train_osh.m
@@ -1,26 +1,7 @@
-function train_osh(Xtrain, Ytrain, run_trial, opts)
+function train_osh(run_trial, opts)
 	% online (semi-)supervised hashing
 
-	% test
-	%{
-	[W, H, ECOCs] = init_osh(Xtrain, opts);
-	Xtest = evalin('caller', 'Xtest');
-	Ytest = evalin('caller', 'Ytest');
-	H = (W'*Xtrain' > 0);
-	Htest = (W'*Xtest' > 0);
-	get_results(H, Htest, Ytrain, Ytest, opts);
-	% test
-	%}
-	
-	% [hack] 
-	exp100dir = sprintf('%s/%s/%dpts_all101tests', opts.localdir, opts.identifier, ...
-		opts.noTrainingPoints);
-	if ~exist(exp100dir, 'dir')
-		myLogInfo('[HACK] Secretly creating %s', exp100dir);
-		mkdir(exp100dir); 
-		if ~opts.windows, unix(['chmod g+w ', exp100dir]); end
-	end
-
+	global Xtrain Ytrain
 	train_time  = zeros(1, opts.ntrials);
 	update_time = zeros(1, opts.ntrials);
 	bit_flips   = zeros(1, opts.ntrials);
@@ -32,41 +13,19 @@ function train_osh(Xtrain, Ytrain, run_trial, opts)
 		myLogInfo('%s: %d trainPts, random trial %d', opts.identifier, opts.noTrainingPoints, t);
 
 		% randomly set test checkpoints (to better mimic real scenarios)
-		% NEW: always use 101 checkpoints (see note below), and require opts.ntests 
-		%      to be a divider of 100. This way when ntests changes for the same 
-		%      parameters, we won't have to run again
-		save_iters = [];
-		interval = round(opts.noTrainingPoints/100*.99);
-		for i = 1:99
+		test_iters      = zeros(1, opts.ntests);
+		test_iters(1)   = 1;
+		test_iters(end) = opts.noTrainingPoints;
+		interval = round(opts.noTrainingPoints/(opts.ntests-1));
+		for i = 1:opts.ntests-2
 			iter = interval*i + randi([1 round(interval/4)]) - round(interval/8);
-			save_iters = [save_iters, iter];
+			test_iters(i+1) = iter;
 		end
-		save_iters = [1, save_iters, opts.noTrainingPoints];  % 101 in total
+		prefix = sprintf('%s/trial%d', opts.expdir, t);
 
 		% do SGD optimization
-		prefix = sprintf('%s/trial%d', exp100dir, t); %[hack]
-		[train_time(t), update_time(t), bit_flips(t)] = sgd_optim(...
-			Xtrain, Ytrain, prefix, save_iters, t, opts);
-
-		% [hack] NEW: test_iters will be a subset of save_iters
-		% now symlink/copy models in exp100dir into opts.expdir
-		% 
-		% NOTE due to some weird technical detail, it's much more convenient to use
-		%      a total of 101 checkpoints (save_iters). 
-		%      Then it gives (ntests+1) equally spaced test_iters
-		%      [fingers crossed...]
-		%
-		M = 100/opts.ntests;  % M is int, ensured by get_opts
-		test_iters = save_iters(1:M:end);
-		for iter = test_iters
-			src = sprintf('%s/trial%d_iter%d.mat', exp100dir, t, iter);
-			dst = sprintf('%s/trial%d_iter%d.mat', opts.expdir, t, iter);
-			symlink_or_copy(src, dst, opts.windows);
-		end
-		% for final model, load, modify test_iters, save
-		d = load(sprintf('%s/trial%d.mat', exp100dir, t, iter));
-		d.test_iters = test_iters;
-		save(sprintf('%s/trial%d.mat', opts.expdir, t, iter), '-struct', 'd');
+		[train_time(t), update_time(t), bit_flips(t)] = sgd_optim(Xtrain, Ytrain, ...
+			prefix, test_iters, t, opts);
 	end
 
 	myLogInfo('Training time (total): %.2f +/- %.2f', mean(train_time), std(train_time));
@@ -75,21 +34,12 @@ function train_osh(Xtrain, Ytrain, run_trial, opts)
 	end
 end
 
-% -------------------------------------------------------------
-function symlink_or_copy(src, dst, windows)
-	if windows
-		% arghhh, we can't use ln -s on Window$, just copy
-		copyfile(src, dst);
-	else
-		% ln -s
-		unix(sprintf('ln -s %s %s', src, dst));
-	end
-end
 
 % -------------------------------------------------------------
-function [train_time, update_time, bitflips] = sgd_optim(...
-		Xtrain, Ytrain, prefix, test_iters, trialNo, opts)
+function [train_time, update_time, bitflips] = sgd_optim(Xtrain, Ytrain, ...
+		prefix, test_iters, trialNo, opts)
 	% optimization via SGD
+	%global Xtest Ytest
 
 	% init
 	[W, H, ECOCs] = init_osh(Xtrain, opts);
@@ -121,21 +71,26 @@ function [train_time, update_time, bitflips] = sgd_optim(...
 
 	% SGD iterations
 	i_ecoc = 1;  M_ecoc = [];  seenLabels = [];
+	num_labeled = 0;
+	num_unlabeled = 0;
 	for i = 1:opts.noTrainingPoints
 		t_ = tic;  
 		% new training point
 		spoint = Xtrain(i, :);
-		slabel = Ytrain(i, :);
+		slabel = Ytrain(i);  % KH: assuming single-labeled data for now
 
-		if sum(slabel) > 0  % labeled: assign target code(s)
+		if slabel > 0  %if sum(slabel) > 0  
+			% labeled: assign target code
 			isLabeled = true;
-			[target_codes, seenLabels, M_ecoc, i_ecoc] = find_target_codes(...
+			num_labeled = num_labeled + 1;
+			[target_code, seenLabels, M_ecoc, i_ecoc] = find_target_codes(...
 				slabel, seenLabels, M_ecoc, i_ecoc, ECOCs);
-		else  % unlabeled
+		else  
+			% unlabeled
 			isLabeled = false;
+			num_unlabeled = num_unlabeled + 1;
 			if opts.reg_maxent > 0  % update maxent regularizer
 				U = U*num_unlabeled + spoint'*spoint;
-				num_unlabeled = num_unlabeled + 1;
 				U = U/num_unlabeled;
 			elseif opts.reg_smooth > 0 && opts.reg_rs > 0
 				% hack: for the reservoir, smooth mapping is assumed
@@ -153,9 +108,10 @@ function [train_time, update_time, bitflips] = sgd_optim(...
 				Xsample, Ysample, priority_queue, spoint, slabel, i, reservoir_size);
 
 			% a hack -we always use smooth mapping for reservoir samples 
-			ropts = opts;
-			ropts.mapping = 'smooth';
-			Hnew = build_hash_table(W, Xsample, Ysample, seenLabels, M_ecoc, ropts)';
+			%ropts = opts;
+			%ropts.mapping = 'smooth';
+			%build_hash_table(W, Xsample, Ysample, seenLabels, M_ecoc, ropts)';
+			Hnew = (W' * Xsample' > 0)';
 			if isempty(Hres)
 				Hres = Hnew;  
 				if strcmp(opts.mapping,'smooth'), update_table = true; end
@@ -183,18 +139,17 @@ function [train_time, update_time, bitflips] = sgd_optim(...
 
 		% SGD-1. update W wrt. loss term(s)
 		if isLabeled
-			for c = 1:size(target_codes, 1)
-				code = target_codes(c, :);
-				W = sgd_update(W, spoint, code, opts.stepsize, opts.SGDBoost);
-			end
+			%for c = 1:size(target_codes, 1)
+			%code = target_codes(c, :);
+			W = sgd_update(W, spoint, target_code, opts.stepsize, opts.SGDBoost);
+			%end
 		end
 
 		% SGD-2. update W wrt. reservoir regularizer (if specified)
-		if isLabeled
-			if opts.reg_rs > 0  &&  i > reservoir_size
-				stepsizes = ones(reservoir_size,1)*opts.reg_rs*opts.stepsize/reservoir_size;
-				W = sgd_update(W, Xsample, Hres, stepsizes, opts.SGDBoost);
-			end
+		% TODO when to use rs.reg.?
+		if isLabeled && opts.reg_rs > 0  &&  i > reservoir_size
+			stepsizes = ones(reservoir_size,1)*opts.reg_rs*opts.stepsize/reservoir_size;
+			W = sgd_update(W, Xsample, Hres, stepsizes, opts.SGDBoost);
 		end
 
 		% SGD-3. update W wrt. unsupervised regularizer (if specified)
@@ -210,20 +165,22 @@ function [train_time, update_time, bitflips] = sgd_optim(...
 		%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 		% hash index update
 		if i == 1 || i == opts.noTrainingPoints 
-			update_table = true;  % update on first & last iteration no matter what
+			% update on first & last iteration no matter what
+			update_table = true;  
 		elseif opts.reg_rs <= 0
 			% NOTE: if using reservoir, update_table is already set.
-			% TODO if mapping is not smooth, set update_interval to
-			% noTrainingPoints
 			update_table = ~mod(i, opts.update_interval);
 		end
-		%if strcmp(opts.mapping, 'smooth') && update_table
 		if update_table
 			t_ = tic;
 			Hnew = build_hash_table(W, Xtrain, Ytrain, seenLabels, M_ecoc, opts);
 			if ~isempty(H)
 				bitdiff = xor(H, Hnew);
-				bitflips = bitflips + sum(bitdiff(:))/ntrain_all;
+				bitdiff = sum(bitdiff(:))/ntrain_all;
+				bitflips = bitflips + bitdiff;
+				myLogInfo('[T%02d] HT update @%d, bitdiff=%g', trialNo, i, bitdiff);
+			else
+				myLogInfo('[T%02d] HT udpate @%d', trialNo, i);
 			end
 			H = Hnew;
 			update_time = update_time + toc(t_);
@@ -232,37 +189,28 @@ function [train_time, update_time, bitflips] = sgd_optim(...
 		%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 		% cache intermediate model to disk
 		if ismember(i, test_iters)
-			savefile = sprintf('%s_iter%d.mat', prefix, i);
-			save(savefile, 'W', 'H', 'bitflips', 'train_time', 'update_time', 'seenLabels');
-			if ~opts.windows, unix(['chmod o-w ' savefile]); end  % matlab permission bug
-		end
-		if ~mod(i, round(opts.noTrainingPoints/5))
-			myLogInfo('Trial %02d, Iter %d/%d. Elapsed: SGD %.2f, HT_update %.2f', ...
-				trialNo, i, opts.noTrainingPoints, train_time, update_time);
+			F = sprintf('%s_iter%d.mat', prefix, i);
+			save(F, 'W', 'H', 'bitflips', 'train_time', 'update_time', 'seenLabels');
+			if ~opts.windows, unix(['chmod o-w ' F]); end  % matlab permission bug
+
+			myLogInfo(['[T%02d] (%d/%d) SGD %.2fs, HTU %.2fs\n' ...
+				'            L=%d, UL=%d, SeenLabels=%d, #BF=%g\n'], ...
+				trialNo, i, opts.noTrainingPoints, train_time, update_time, ...
+				num_labeled, num_unlabeled, numel(seenLabels), bitflips);
+
+			% DEBUG
+			%Htest = (W'*Xtest' > 0);
+			%get_results(H, Htest, Ytrain, Ytest, opts);
+			%disp('.');
+			% DEBUG
 		end
 	end % end for
 
-	if opts.reg_rs > 0
-		myLogInfo('Trial %02d. bitflips_res = %g', trialNo, bitflips_res);
-	end
-	if opts.reg_maxent > 0
-		myLogInfo('Trial %02d. %d labeled, %d unlabeled. reg_maxent = %g', ...
-			trialNo, opts.noTrainingPoints-num_unlabeled, num_unlabeled, opts.reg_maxent);
-	end
-
-	%{ KH: obsolete now because we now always update on last iteration
-	% populate hash table
-	t_ = tic;
-	H = build_hash_table(W, Xtrain, Ytrain, seenLabels, M_ecoc, opts);
-	update_time = update_time + toc(t_);
-	%}
-	myLogInfo('Trial %02d. TOTAL: SGD %.2f sec, HT_update %.2f sec', ...
-		trialNo, train_time, update_time);
-
 	% save final model, etc
-	save([prefix '.mat'], 'W', 'H', 'bitflips', 'train_time', 'update_time', 'test_iters');
-	if ~opts.windows, unix(['chmod o-w ' prefix '.mat']); end % matlab permission bug
-	myLogInfo('Saved %s.mat', prefix);
+	F = [prefix '.mat'];
+	save(F, 'W', 'H', 'bitflips', 'train_time', 'update_time', 'test_iters');
+	if ~opts.windows, unix(['chmod o-w ' F]); end % matlab permission bug
+	myLogInfo('[T%02d] Saved: %s\n', trialNo, F);
 end
 
 % -----------------------------------------------------------
@@ -309,6 +257,8 @@ end
 function [W, H, ECOCs] = init_osh(Xtrain, opts, bigM)
 	% randomly generate candidate codewords, store in ECOCs
 	if nargin < 3, bigM = 10000; end
+			
+	% NOTE ECOCs now is a BINARY (0/1) MATRIX!
 	ECOCs = logical(zeros(bigM, opts.nbits));
 	for t = 1:opts.nbits
 		r = ones(bigM, 1);
@@ -332,22 +282,17 @@ function [target_codes, seenLabels, M_ecoc, i_ecoc] = find_target_codes(...
 		slabel, seenLabels, M_ecoc, i_ecoc, ECOCs);
 	assert(sum(slabel) ~= 0, 'Error: finding target codes for unlabeled example');
 
-	if numel(slabel) == 1  % single-label dataset
-		ind = find(seenLabels == slabel);
-		if isempty(ind)
-			if isempty(seenLabels)
-				% does not exist, create a binary code for M_ecoc
-				seenLabels = slabel;
-				M_ecoc = ECOCs(i_ecoc, :);
-				i_ecoc = i_ecoc + 1;
-			else
-				% append codeword to ECOC matrix
-				seenLabels = [seenLabels; slabel];
-				M_ecoc = [M_ecoc; ECOCs(i_ecoc,:)];
-				i_ecoc = i_ecoc +1;
-			end
+	if numel(slabel) == 1  
+		% single-label dataset
+		[ismem, ind] = ismember(slabel, seenLabels);
+		if ismem == 0
+			seenLabels = [seenLabels; slabel];
+			% NOTE ECOCs now is a BINARY (0/1) MATRIX!
+			M_ecoc = [M_ecoc; 2*ECOCs(i_ecoc,:)-1];
+			ind    = i_ecoc;
+			i_ecoc = i_ecoc + 1;
 		end
-		ind = find(seenLabels == slabel);
+
 	else  
 		% multi-label dataset
 		if isempty(seenLabels) 
@@ -359,13 +304,15 @@ function [target_codes, seenLabels, M_ecoc, i_ecoc] = find_target_codes(...
 		unseen = find((slabel==1) & (seenLabels==0));
 		if ~isempty(unseen)
 			for j = unseen
-				M_ecoc(j, :) = ECOCs(i_ecoc, :);
+				% NOTE ECOCs now is a BINARY (0/1) MATRIX!
+				M_ecoc(j, :) = 2*ECOCs(i_ecoc, :)-1;
 				i_ecoc = i_ecoc + 1;
 			end
 			seenLabels(unseen) = 1;
 		end
 		ind = find(slabel==1);
 	end
+
 	% find/assign target codes
 	target_codes = M_ecoc(ind, :);
 end
@@ -403,5 +350,3 @@ function W = reg_smooth(W, points, reg_smooth)
 		end
 	end
 end
-
-

test_osh.m
@@ -1,6 +1,18 @@
-function test_osh(Xtest, Ytest, Ytrain, resfn, res_trial_fn, res_exist, opts)
+function test_osh(resfn, res_trial_fn, res_exist, opts)
 	% if we're running this function, it means some elements in res_exist is false
 	% and we need to compute/recompute the corresponding res_trial_fn's
+	global Xtest Ytest Ytrain
+
+	% handle test_frac
+	if opts.test_frac < 1
+		myLogInfo('! only testing first %g%%', opts.test_frac*100);
+		idx = 1:round(size(Xtest, 1)*opts.test_frac);
+		testX = Xtest(idx, :);
+		testY = Ytest(idx);
+	else
+		testX = Xtest;
+		testY = Ytest;
+	end
 
 	% for semi-supervised case, only do retrieval against LABELED training data
 	% NOTE assuming single-labeled examples
@@ -32,11 +44,11 @@ function test_osh(Xtest, Ytest, Ytrain, resfn, res_trial_fn, res_exist, opts)
 				end
 				if 0 %~isempty(d.seenLabels)
 					[~, ind] = ismember(d.seenLabels, Ytest);
-					Htest = (d.W'*Xtest(ind, :)' > 0);
-					Ltest = Ytest(ind);
+					Htest = (d.W'*testX(ind, :)' > 0);
+					Ltest = testY(ind);
 				else
-					Htest = (d.W'*Xtest' > 0);
-					Ltest = Ytest;
+					Htest = (d.W'*testX' > 0);
+					Ltest = testY;
 				end
 
 				fprintf('Trial %d, Iter %5d/%d, ', t, iter, opts.noTrainingPoints);
@@ -45,6 +57,7 @@ function test_osh(Xtest, Ytest, Ytrain, resfn, res_trial_fn, res_exist, opts)
 				t_train_iter(i) = iter;
 				t_train_time(i) = d.train_time;
 			end
+			clear Htrain Htest Ltest
 			save(res_trial_fn{t}, 't_res', 't_bitflips', 't_train_iter', 't_train_time');
 		end
 		res(t, :) = t_res;