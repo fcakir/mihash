7f8f99727b4c0e1a7b42a7a92465c86082d56521
Kun He <hekun@cs.bu.edu>
2016-10-03 21:18:42 -0400

    indent update for demo's

--- demo_osh.m
+++ demo_osh.m
@@ -1,82 +1,81 @@
 function resfn = demo_osh(ftype, dataset, nbits, varargin)
-	% PARAMS
-	%  ftype (string) from {'gist', 'cnn'}
-	%  dataset (string) from {'cifar', 'sun','nus'}
-	%  nbits (integer) is length of binary code
-	%  varargin: see get_opts.m for details
-	%  
-	opts = get_opts(ftype, dataset, nbits, varargin{:});  % set parameters
+% PARAMS
+%  ftype (string) from {'gist', 'cnn'}
+%  dataset (string) from {'cifar', 'sun','nus'}
+%  nbits (integer) is length of binary code
+%  varargin: see get_opts.m for details
+%  
+opts = get_opts(ftype, dataset, nbits, varargin{:});  % set parameters
 
-	% 0. result files
-	Rprefix = sprintf('%s/%s', opts.expdir, opts.metric);
-	if opts.testFrac < 1
-		Rprefix = sprintf('%s_frac%g', Rprefix);
-	end
+% 0. result files
+Rprefix = sprintf('%s/%s', opts.expdir, opts.metric);
+if opts.testFrac < 1
+    Rprefix = sprintf('%s_frac%g', Rprefix);
+end
 
-	resfn = sprintf('%s_%dtrials.mat', Rprefix, opts.ntrials);
-	res_trial_fn = cell(1, opts.ntrials);
-	for t = 1:opts.ntrials 
-		res_trial_fn{t} = sprintf('%s_trial%d.mat', Rprefix, t);
-	end
-	if opts.override
-		res_exist = zeros(1, opts.ntrials);
-	else
-		res_exist = cellfun(@(r) exist(r, 'file'), res_trial_fn);
-	end
-
-
-	% 1. determine which (training) trials to run
-	if opts.override
-		run_trial = ones(1, opts.ntrials);
-	else
-		run_trial = zeros(1, opts.ntrials);
-		for t = 1:opts.ntrials
-			if exist(res_trial_fn{t}, 'file')
-				run_trial(t) = 0;
-			else
-				% [hack] for backward compatibility:
-				% if final model trial_%d.mat exists and all the intermediate models 
-				% exist as well, then we did this trial previously, just didn't save
-				% res_trial_fn{t} -- NO need to rerun training
-				modelprefix = sprintf('%s/trial%d', opts.expdir, t);
-				try
-					model = load([modelprefix '.mat']);
-					model_exist = arrayfun(@(i) ...
-						exist(sprintf('%s_iter%d.mat', modelprefix, i), 'file'), ...
-						model.test_iters);
-					if all(model_exist), run_trial(t) = 0; 
-					else, run_trial(t) = 1; end
-				catch
-					run_trial(t) = 1;
-				end
-			end
-		end
-	end
+resfn = sprintf('%s_%dtrials.mat', Rprefix, opts.ntrials);
+res_trial_fn = cell(1, opts.ntrials);
+for t = 1:opts.ntrials 
+    res_trial_fn{t} = sprintf('%s_trial%d.mat', Rprefix, t);
+end
+if opts.override
+    res_exist = zeros(1, opts.ntrials);
+else
+    res_exist = cellfun(@(r) exist(r, 'file'), res_trial_fn);
+end
 
 
-	% 2. load data (only if necessary)
-	global Xtrain Xtest Ytrain Ytest Dtype
-	Dtype_this = [dataset '_' ftype];
-	if ~isempty(Dtype) && strcmp(Dtype_this, Dtype)
-		myLogInfo('Dataset already loaded for %s', Dtype_this);
-	elseif (any(run_trial) || ~all(res_exist))
-		myLogInfo('Loading data for %s...', Dtype_this);
-		eval(['[Xtrain, Ytrain, Xtest, Ytest] = load_' opts.ftype '(dataset, opts);']);
-		Dtype = Dtype_this;
-	end
+% 1. determine which (training) trials to run
+if opts.override
+    run_trial = ones(1, opts.ntrials);
+else
+    run_trial = zeros(1, opts.ntrials);
+    for t = 1:opts.ntrials
+        if exist(res_trial_fn{t}, 'file')
+            run_trial(t) = 0; continue;
+        end
+        % [hack] for backward compatibility:
+        % if final model trial_%d.mat exists and all the intermediate models 
+        % exist as well, then we did this trial previously, just didn't save
+        % res_trial_fn{t} -- NO need to rerun training
+        modelprefix = sprintf('%s/trial%d', opts.expdir, t);
+        try
+            model = load([modelprefix '.mat']);
+            model_exist = arrayfun(@(i) ...
+                exist(sprintf('%s_iter%d.mat', modelprefix, i), 'file'), ...
+                model.test_iters);
+            if all(model_exist), run_trial(t) = 0; 
+            else, run_trial(t) = 1; end
+        catch
+            run_trial(t) = 1;
+        end
+    end
+end
 
-	% 3. TRAINING: run all _necessary_ trials (handled by train_osh)
-	if any(run_trial)
-		myLogInfo('Training models...');
-		train_osh(run_trial, opts);
-	end
-	myLogInfo('Training is done.');
 
-	% 4. TESTING: run all _necessary_ trials
-	if ~all(res_exist) || ~exist(resfn, 'file')
-		myLogInfo('Testing models...');
-		test_osh(resfn, res_trial_fn, res_exist, opts);
-	end
-	myLogInfo('Testing is done.');
-    diary('off')
+% 2. load data (only if necessary)
+global Xtrain Xtest Ytrain Ytest Dtype
+Dtype_this = [dataset '_' ftype];
+if ~isempty(Dtype) && strcmp(Dtype_this, Dtype)
+    myLogInfo('Dataset already loaded for %s', Dtype_this);
+elseif (any(run_trial) || ~all(res_exist))
+    myLogInfo('Loading data for %s...', Dtype_this);
+    eval(['[Xtrain, Ytrain, Xtest, Ytest] = load_' opts.ftype '(dataset, opts);']);
+    Dtype = Dtype_this;
+end
+
+% 3. TRAINING: run all _necessary_ trials (handled by train_osh)
+if any(run_trial)
+    myLogInfo('Training models...');
+    train_osh(run_trial, opts);
+end
+myLogInfo('Training is done.');
+
+% 4. TESTING: run all _necessary_ trials
+if ~all(res_exist) || ~exist(resfn, 'file')
+    myLogInfo('Testing models...');
+    test_osh(resfn, res_trial_fn, res_exist, opts);
+end
+myLogInfo('Testing is done.');
+diary('off')
 end
--- demo_retrieval.m
+++ demo_retrieval.m
@@ -1,54 +1,54 @@
 function demo_retrieval(dataset)
 
-	% assuming CNN features and 32 bits
-	if strcmp(dataset, 'cifar')
-		opts = get_opts('cnn', dataset, 32, 'ntrials', 1, 'ntests', 2, ...
-			'noTrainingPoints', 50e3, 'updateInterval', 50e3, 'showplots', 0, ...
-			'nworkers', 0);
-		visFunc = @vis_cifar;
-	else
-		opts = get_opts('cnn', 'places', 32, 'ntrials', 1, 'ntests', 2, ...
-			'noTrainingPoints', 50e3, 'updateInterval', 50e3, 'showplots', 0, ...
-			'labelspercls', 0, 'metric', 'prec_n3', 'nworkers', 0);
-		visFunc = @vis_places;
-	end
+% assuming CNN features and 32 bits
+if strcmp(dataset, 'cifar')
+    opts = get_opts('cnn', dataset, 32, 'ntrials', 1, 'ntests', 2, ...
+        'noTrainingPoints', 50e3, 'updateInterval', 50e3, 'showplots', 0, ...
+        'nworkers', 0);
+    visFunc = @vis_cifar;
+else
+    opts = get_opts('cnn', 'places', 32, 'ntrials', 1, 'ntests', 2, ...
+        'noTrainingPoints', 50e3, 'updateInterval', 50e3, 'showplots', 0, ...
+        'labelspercls', 0, 'metric', 'prec_n3', 'nworkers', 0);
+    visFunc = @vis_places;
+end
 
 
-	% load data (only if necessary)
-	% Names: struct of two fields, 'train' & 'test'
-	global Xtrain Xtest Ytrain Ytest Names Dtype
-	Dtype_this = [dataset '_' opts.ftype];
-	if ~isempty(Dtype) && strcmp(Dtype_this, Dtype)
-		myLogInfo('Dataset already loaded for %s', Dtype_this);
-	else
-		myLogInfo('Loading data for %s...', Dtype_this);
-		eval(['[Xtrain, Ytrain, Xtest, Ytest, Names] = load_', opts.ftype, ...
-			'(dataset, opts);']);
-		Dtype = Dtype_this;
-	end
+% load data (only if necessary)
+% Names: struct of two fields, 'train' & 'test'
+global Xtrain Xtest Ytrain Ytest Names Dtype
+Dtype_this = [dataset '_' opts.ftype];
+if ~isempty(Dtype) && strcmp(Dtype_this, Dtype)
+    myLogInfo('Dataset already loaded for %s', Dtype_this);
+else
+    myLogInfo('Loading data for %s...', Dtype_this);
+    eval(['[Xtrain, Ytrain, Xtest, Ytest, Names] = load_', opts.ftype, ...
+        '(dataset, opts);']);
+    Dtype = Dtype_this;
+end
 
 
-	% run training
-	modelfn = sprintf('%s/trial1.mat', opts.expdir);
-	if ~exist(modelfn, 'file')
-		myLogInfo('Training model...');
-		run_trial = 1;
-		train_osh(run_trial, opts);
-	else
-		myLogInfo('Already trained');
-	end
+% run training
+modelfn = sprintf('%s/trial1.mat', opts.expdir);
+if ~exist(modelfn, 'file')
+    myLogInfo('Training model...');
+    run_trial = 1;
+    train_osh(run_trial, opts);
+else
+    myLogInfo('Already trained');
+end
 
 
-	% get results
-	d = load(modelfn);
-	Htrain = d.H;
-	Htest  = (d.W' * Xtest' > 0);
-	sim = (2*Htrain-1)'*(2*Htest-1);
+% get results
+d = load(modelfn);
+Htrain = d.H;
+Htest  = (d.W' * Xtest' > 0);
+sim = (2*Htrain-1)'*(2*Htest-1);
 
 
-	% visualize
-	if ~exist('res', 'dir'), mkdir('res'); end
-	visFunc(sim, floor(Ytrain/10), floor(Ytest/10), Names);
+% visualize
+if ~exist('res', 'dir'), mkdir('res'); end
+visFunc(sim, floor(Ytrain/10), floor(Ytest/10), Names);
 
 end
 
@@ -56,83 +56,83 @@ end
 % ---------------------------------------------------------------
 function vis_places(sim, trainY, testY, Names)
 
-	% sample some test images
-	K = 20; %(?)
-	for i = randperm(length(testY), K)
-		name = Names.test{i};
-		fn = sprintf('res/query%d.png', i);
-		try
-			web_save(name, fn);
-		catch, name, continue
-		end
-		myLogInfo('saved %s', fn);
+% sample some test images
+K = 20; %(?)
+for i = randperm(length(testY), K)
+    name = Names.test{i};
+    fn = sprintf('res/query%d.png', i);
+    try
+        web_save(name, fn);
+    catch, name, continue
+    end
+    myLogInfo('saved %s', fn);
 
-		% get top 10 results
-		myLogInfo('Label %d, top 10 retrieved labels:', testY(i));
-		[val, ind] = sort(sim(:, i), 'descend');
+    % get top 10 results
+    myLogInfo('Label %d, top 10 retrieved labels:', testY(i));
+    [val, ind] = sort(sim(:, i), 'descend');
 
-		for j = 1:10
-			fprintf('%d ', trainY(ind(j)));
-			name = Names.train{ind(j)};
-			fn = sprintf('res/query%d_retrieval%02d.png', i, j);
-			try
-				web_save(name, fn);
-			catch, name, continue;
-			end
-		end
-		fprintf('\n\n');
-	end
+    for j = 1:10
+        fprintf('%d ', trainY(ind(j)));
+        name = Names.train{ind(j)};
+        fn = sprintf('res/query%d_retrieval%02d.png', i, j);
+        try
+            web_save(name, fn);
+        catch, name, continue;
+        end
+    end
+    fprintf('\n\n');
+end
 end
 
 
 function web_save(name, fn)
-	prefix = 'http://monday.csail.mit.edu/cmr/RGB';
+prefix = 'http://monday.csail.mit.edu/cmr/RGB';
 
-	[path, imfn, ext] = fileparts(name);  % get image name
-	[path, cls, ~] = fileparts(path);  % get class name
-	[path, c, ~] = fileparts(path);  % get preceeding string
-	if length(c) == 1  % single character
-		url = sprintf('%s/%s/%s/%s', prefix, c, cls, [imfn ext]);
-	else
-		supercls = c;
-		[path, c, ~] = fileparts(path);
-		url = sprintf('%s/%s/%s/%s/%s', prefix, c, supercls, cls, [imfn ext]);
-	end
+[path, imfn, ext] = fileparts(name);  % get image name
+[path, cls, ~] = fileparts(path);  % get class name
+[path, c, ~] = fileparts(path);  % get preceeding string
+if length(c) == 1  % single character
+    url = sprintf('%s/%s/%s/%s', prefix, c, cls, [imfn ext]);
+else
+    supercls = c;
+    [path, c, ~] = fileparts(path);
+    url = sprintf('%s/%s/%s/%s/%s', prefix, c, supercls, cls, [imfn ext]);
+end
 
-	urlwrite(url, fn);
+urlwrite(url, fn);
 end
 
 % ---------------------------------------------------------------
 function vis_cifar(sim, trainY, testY, Names)
-        % load cifar images
-        load('/research/cbi/sbargal_work_dir/CIFAR/cifar_dataset.mat');
-	% sample one(?) test image from each class 
-	K = 2;
-	for y = 1:10
-		ind = find(testY == y);
-		ind = ind(randperm(length(ind), K));
+% load cifar images
+load('/research/cbi/sbargal_work_dir/CIFAR/cifar_dataset.mat');
+% sample one(?) test image from each class 
+K = 2;
+for y = 1:10
+    ind = find(testY == y);
+    ind = ind(randperm(length(ind), K));
 
-		for i = ind'
-			%name = Names.test{i};
-	                fn = sprintf('query%d.png', i);
-		
-			im = reshape(cifarImgs(cell2mat(Names.test{i}),:),32,32,3);
-        	        imwrite(im, fn);
+    for i = ind'
+        %name = Names.test{i};
+        fn = sprintf('query%d.png', i);
 
-			myLogInfo('Label %d, top 10 retrieved labels:', testY(i));
+        im = reshape(cifarImgs(cell2mat(Names.test{i}),:),32,32,3);
+        imwrite(im, fn);
 
-			% get top 10 results
-			[val, ind] = sort(sim(:, i), 'descend');
+        myLogInfo('Label %d, top 10 retrieved labels:', testY(i));
 
-			for j = 1:10
-				fprintf('%d ', trainY(ind(j)));
-				% im = somehow_get_image( Names.train(ind(j)) );
-				im = reshape(cifarImgs(cell2mat(Names.train{ind(j)}),:),32,32,3);
-				fn = sprintf('query%d_retrieval%d.png', i, j);
-				imwrite(im, fn);
-			end
-			fprintf('\n\n');
-		end
-	end
+        % get top 10 results
+        [val, ind] = sort(sim(:, i), 'descend');
+
+        for j = 1:10
+            fprintf('%d ', trainY(ind(j)));
+            % im = somehow_get_image( Names.train(ind(j)) );
+            im = reshape(cifarImgs(cell2mat(Names.train{ind(j)}),:),32,32,3);
+            fn = sprintf('query%d_retrieval%d.png', i, j);
+            imwrite(im, fn);
+        end
+        fprintf('\n\n');
+    end
+end
 end
 
