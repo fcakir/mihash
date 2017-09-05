e75bb4fe27e7d6b17eb32da7c1a1b9ba7ff259af
Kun He <hekun@cs.bu.edu>
2017-09-04 23:09:03 -0400

    + download_data

---
+++ data/download_data.sh
@@ -0,0 +1,3 @@
+wget http://cs-people.bu.edu/hekun/data/mihash/CIFAR10_VGG16_fc7.mat
+wget http://cs-people.bu.edu/hekun/data/mihash/Places205_AlexNet_fc7_PCA128.mat
+wget http://cs-people.bu.edu/hekun/data/mihash/LabelMe_GIST.mat
