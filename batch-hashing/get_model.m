3b59f0f77311d5f00441e5af99d3bc2f07d033a9
Kun He <kunhe26@gmail.com>
2017-09-11 22:21:24 -0400

    IMDB and Models

--- batch-hashing/get_model.m
+++ batch-hashing/get_model.m
@@ -39,7 +39,7 @@ function [net, opts] = get_model(opts)
 %
 %------------------------------------------------------------------------------
 t0 = tic;
-modelFunc = str2func(sprintf('models.%s', opts.modelType));
+modelFunc = str2func(sprintf('Models.%s', opts.modelType));
 [net, opts.imageSize, opts.normalize] = modelFunc(opts);
 myLogInfo('%s in %.2fs', opts.modelType, toc(t0));
 end
